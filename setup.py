d4217dd97771e6dfc981316db34093ddc4274482
sinarezaei1991@gmail.com <Harpalus%123>
2022-09-18 15:59:55 +0430

    Add setup.py Update gitignore file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(name='alerting',
+                 version='0.0.9.1',
+                 author='Sina Rezaei',
+                 author_email='sinarezaei1991@gmail.com',
+                 long_description_content_type="text/markdown",
+                 long_description=long_description,
+                 description='Alerting: Easy to Use alerting library for Python',
+                 url='https://github.com/sinarezaei/alerting',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 install_requires=required,
+                 python_requires='>=3.6',
+                 zip_safe=False)
